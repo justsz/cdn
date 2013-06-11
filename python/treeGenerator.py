6b9cd0d6c4c95c96dcbd01e401fcc2688e216b2a
justsz <justs.zarins@gmail.com>
2013-06-11 18:57:26 +0100

    added innter node selector. link info is now linked to node.

--- python/treeGenerator.py
+++ python/treeGenerator.py
@@ -5,8 +5,8 @@ import random
 
 
 print("input node count")
-nodeCount = raw_input() / 2
-nodeCount = int(nodeCount)
+nodeCount = raw_input()
+nodeCount = int(nodeCount) / 2
 
 print("output filename")
 fileName = raw_input()
@@ -17,6 +17,11 @@ k = "children"
 l = "length"
 n = "name"
 
+wordList = []
+file = open("nouns.txt")
+
+for line in file:
+	wordList.append(line.rstrip()) #strip newline characters
 
 tree = {}
 
@@ -25,10 +30,13 @@ def getChildren (node):
                  return node[k]
              else:
                  return None
+                 
+def sampleWord (wordList):
+	return wordList[random.randint(0, len(wordList)-1)]
 
 
 for x in range(0, nodeCount):
-    print(x)
+    #print(x)
     cont = True
     node = tree
     children = getChildren(node)
@@ -41,7 +49,7 @@ for x in range(0, nodeCount):
 	        node[k] = []
 	        children = getChildren(node)
 	        children.append({n : str(x), l: random.random()})
-        	children.append({n : str(x+nodeCount), l: random.random()})
+        	children.append({n : str(x+nodeCount*2), l: random.random()})
     	    #print("appending")
 	        cont = False
     

5f18d17d68cfeabfd7526b84277ec5b098ec70a5
Justs Zarins <justs.zarins@gmail.com>
2013-09-16 17:38:19 +0100

    added release folder

--- javascript/combineFiles.sh
+++ javascript/combineFiles.sh
@@ -6,10 +6,11 @@ cat legendPanel.js >> pandemix-min.js
 cat timePanel.js >> pandemix-min.js
 cat mapPanel.js >> pandemix-min.js
 cat regionOutlineMapLayer.js >> pandemix-min.js
-cat centroidMapLayer.js >> pandemix-min.js
+cat locationMapLayer.js >> pandemix-min.js
 cat virusParticleMapLayer.js >> pandemix-min.js
 cat bubbleChartMapLayer.js >> pandemix-min.js
 cat bubbleTransMapLayer.js >> pandemix-min.js
+cat treeMapLayer.js >> pandemix-min.js
 java -jar yuicompressor-2.4.7.jar pandemix-min.js -o pandemix-min.js
 cat d3.v3.min.js > allMin.js
 cat crossfilter.min.js >> allMin.js
